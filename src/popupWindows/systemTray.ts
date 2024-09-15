1a842a3250c5aac0a0beea4edd2876528dfccf41
coolguy1842 <kian.goslett@protonmail.com>
2024-09-15 16:28:51 +1000

    make it hide when the popup is empty

--- src/popupWindows/systemTray.ts
+++ src/popupWindows/systemTray.ts
@@ -5,20 +5,41 @@ import { PopupAnimations } from "src/utils/classes/PopupAnimation";
 import { PopupWindow } from "src/utils/classes/PopupWindow";
 import { Binding } from "types/service";
 import { Option } from "src/utils/handlers/optionsHandler";
+import { TrayItem } from "resource:///com/github/Aylur/ags/service/systemtray.js";
+import { getActiveFavorites, getTrayItemID } from "src/utils/utils";
 
 const tray = await Service.import("systemtray");
 
 function createTrayFavoritesPopupWidget(favorites: Option<string[]>, icon_size: number | Binding<any, any, number>) {
-    const updateFn = (self) => {
-        updateTray(self, TrayType.NON_FAVORITES, favorites.value, icon_size, () => {});
+    const onMiddleClick = (item: TrayItem) => {
+        favorites.value = [
+            ...new Set([
+                ...favorites.value,
+                getTrayItemID(item)
+            ])
+        ]
     }
 
-    return Widget.Box({
+    const updateFn = (self) => {
+        updateTray(self, TrayType.NON_FAVORITES, favorites.value, icon_size, onMiddleClick);
+    }
+
+
+    const tryHide = () => {
+        if(getActiveFavorites(favorites.value).length == tray.items.length) {
+            TrayFavoritesPopupWindow.hide();
+        }
+    }
+
+
+    const widget = Widget.Box({
         className: "system-tray",
         setup: updateFn
     })
-        .hook(tray, updateFn)
-        .hook(favorites, updateFn);
+        .hook(tray, (self) => { updateFn(self); tryHide(); })
+        .hook(favorites, (self) => { updateFn(self); tryHide(); });
+
+    return widget;
 }
 
 function createTrayFavoritesPopupWindow() {
